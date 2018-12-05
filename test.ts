cadaf24cda1c90197599f43fd19b49855e02b5b2
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-05 08:45:40 -0800

    adding tests

--- test.ts
+++ test.ts
@@ -1,4 +1,8 @@
-jacdac.broadcastBatteryLevel(function () {
-    return 0;
-})
 jacdac.accelerometer.setStreaming(true);
+jacdac.microhpone.setStreaming(true);
+jacdac.lightSensor.setStreaming(true);
+jacdac.button.setStreaming(true);
+jacdac.touch.setStreaming(true);
+jacdac.monitorBatteryLevels(function (serialNumber: number, level: number) {
+    console.log(`bat ${serialNumber} ${level}`)  
+})
