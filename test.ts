e3a78d7ab78843a2dbde073269db7e9c579ea373
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-06 18:51:36 -0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
-jacdac.accelerometer.setStreaming(true);
-jacdac.microhpone.setStreaming(true);
-jacdac.lightSensor.setStreaming(true);
-jacdac.button.setStreaming(true);
-jacdac.touch.setStreaming(true);
-jacdac.monitorBatteryLevels(function (serialNumber: number, level: number) {
-    console.log(`bat ${serialNumber} ${level}`)
-})
+ 
