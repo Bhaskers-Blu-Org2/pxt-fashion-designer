61cbf580e9d2a6dfa91cb231108633a9c4fe8b01
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-16 11:38:27 -0700

    light sensor

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  * Bead stuff
  */
 //% color=#0c0c00 icon="\uf111" weight=100
-//% groups='["Motion", "Light"]'
+//% groups='["Motion", "Light Sensor" "Light"]'
 namespace fashion {
     //% fixedInstances
     export class Bead {
@@ -12,10 +12,6 @@ namespace fashion {
             this._client = client;
         }
 
-        /**
-         * Gets the name of the bead on the JACDAC network
-         */
-        //% group="Properties"
         get name() {
             return this._client.name;
         }
@@ -25,6 +21,9 @@ namespace fashion {
         }
     }
 
+    /**
+     * A motion detector bead
+     */
     //% fixedInstances
     export class MotionBead extends Bead {
         constructor(name: string) {
@@ -50,11 +49,66 @@ namespace fashion {
     //% fixedInstance whenUsed block="motion bead"
     export const motionBead = new MotionBead("motion bead");
 
+    /**
+     * A light sensor bead
+     */
+    //% fixedInstances
+    export class LightSensorBead extends Bead {
+        constructor(name: string) {
+            super(new jacdac.LightSensorClient(name));
+        }
+
+        /**
+         * Runs code when an event happens on the sensor
+         * @param gesture 
+         * @param handler 
+         */
+        //% blockId=beadslightsensoronevent block="jacdac %lightsensor on %lightCondition"
+        //% group="Light sensor"
+        onEvent(lightCondition: JacdacLightCondition, handler: () => void) {
+            const c = this.client as jacdac.LightSensorClient;
+            c.onEvent(lightCondition, handler);
+        }
+
+        /**
+         * Sets the threshold value for the event
+         * @param level 
+         * @param value 
+         */
+        //% blockId=beadslightsetthrshold block="jacdac %lightsensor set threshold %level to %value"
+        //% group="Light sensor"
+        setLightConditionThreshold(level: JacdacLightCondition, value: number) {
+            const c = this.client as jacdac.LightSensorClient;
+            c.setLightConditionThreshold(level, value);
+        }
+    }
+
+    /**
+     * Any light bead attached to the garment
+     */
+    //% fixedInstance whenUsed block="light sensor bead"
+    export const lightSensorBead = new LightSensorBead("light sensor bead");
+
+    /**
+     * A programmable light bead
+     */
     //% fixedInstances
     export class LightBead extends Bead {
         constructor(name: string) {
             super(new jacdac.LightClient(name));
         }
+
+        /**
+         * Set all of the pixels on the strip to one RGB color.
+         * @param rgb RGB color of the LED
+         */
+        //% blockId="beadlight_set_strip_color" block="set %strip all pixels to %rgb=colorNumberPicker"
+        //% weight=80 blockGap=8
+        //% group="Light"
+        setAll(rgb: number) {
+            const c = this.client as jacdac.LightClient;
+            c.setAll(rgb);
+        }        
 
         /**
          * Show an animation or queue an animation in the animation queue
