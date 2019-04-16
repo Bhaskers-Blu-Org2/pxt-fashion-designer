8035029b2a2fb124d5a7fdfa2df9b364ff97ce36
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-16 08:47:42 -0700

    adding light client

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
-enum BeadGesture {
-    //% block="shake"
-    Shake = JDGesture.Shake
-}
-
 /**
  * Bead stuff
  */
 //% color=#0c0c00 icon="\uf111" weight=100
+//% groups='["Motion", "Light"]'
 namespace fashion {
     //% fixedInstances
     export class Bead {
@@ -20,7 +16,6 @@ namespace fashion {
          * Gets the name of the bead on the JACDAC network
          */
         //% group="Properties"
-        //% blockId=beadname block="%bead name"
         get name() {
             return this._client.name;
         }
@@ -41,18 +36,43 @@ namespace fashion {
          * @param gesture 
          * @param handler 
          */
-        //% blockId=motionbeadongesture block="on motion %bead %gesture"
+        //% blockId=motionbeadongesture block="on %bead %gesture"
         //% group="Motion"
-        onGesture(gesture: BeadGesture, handler: () => void) {
+        onGesture(gesture: JDGesture, handler: () => void) {
             const c = this.client as jacdac.AccelerometerClient;
-            c.onEvent(<JDGesture><number>gesture, handler);
+            c.onEvent(gesture, handler);
         }
     }
 
-    // code to be generated...
     /**
      * Any motion bead attached to the garment
      */
-    //% fixedInstance whenUsed block="any"
-    export const motionAny = new MotionBead("any");
+    //% fixedInstance whenUsed block="motion bead"
+    export const motionBead = new MotionBead("motion bead");
+
+    //% fixedInstances
+    export class LightBead extends Bead {
+        constructor(name: string) {
+            super(new jacdac.LightClient(name));
+        }
+
+        /**
+         * Show an animation or queue an animation in the animation queue
+         * @param animation the animation to run
+         * @param duration the duration to run in milliseconds, eg: 500
+         */
+        //% blockId=beadlight_show_animation block="show %strip animation %animation for %duration=timePicker ms"
+        //% weight=90 blockGap=8
+        //% group="Light"
+        showAnimation(animation: JDLightAnimation, duration: number) {
+            const c = this.client as jacdac.LightClient;
+            c.showAnimation(animation, 500)
+        }
+    }
+
+    /**
+     * Any light bead attached to the garment
+     */
+    //% fixedInstance whenUsed block="light bead"
+    export const lightBead = new LightBead("light bead");
 }
